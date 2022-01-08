c0b99f1999208351692e2143b44be5355138e9d2
AEdgejay <471021444@qq.com>
2022-01-08 22:14:52 +0800

    add development and production config

--- public/config.js
+++ public/config.js
@@ -1,6 +1,9 @@
 if (window.ZEle === undefined) {
     window.ZEle = {};
   }
+  // 生产环境（客户环境）
+  // window.ZEle.endpoint = "";
+  // 开发环境（本地环境)
   window.ZEle.endpoint = "http://repo.dev.smallsaas.cn:8087/";
   window.ZEle.nav = "left"; // LeftNavCollaps left top both
   window.ZEle.indexPage = "";
