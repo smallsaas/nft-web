c8c0a8be6162be381f438430a94a032ff3046ea6
HDJ <263463764@qq.com>
2022-01-10 11:12:23 +0800

    default

--- src/components/FormItemType/InvitationCodeCompx.js
+++ src/components/FormItemType/InvitationCodeCompx.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Input, Button, Space } from 'antd';
 
-export default ({ props, ...rest }) => {
+export default (props) => {
+
+    const { ...rest } = props;
+
+    const { dateFormat = {} } = global;
+    const { value, options = {}, onChange,
+      props: propsOpt,
+      formdata,
+      name,
+      handle,
+      // ...restProps
+      defaultValue,
+      data = {},
+      field,
+      bindfield,
+    } = props;
+  
 
     function send(){
-        console.log('props = ', props)
-        console.log('rest = ', rest)
+        // console.log('formdata = ', formdata)
+        // console.log('name = ', name)
+        // console.log('handle = ', handle)
+        // console.log('defaultValue = ', defaultValue)
+        // console.log('data = ', data)
     }
 
     return (
 
         <Space>
-            <Input {...rest} {...props} style={{ width:"240px" }} />
+            <Input {...rest} {...propsOpt} style={{ width:"240px" }} />
             <Button  fill='outline' color='primary' onClick={() => send()}>获取验证码</Button>
         </Space>
     )
