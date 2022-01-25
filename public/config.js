913cb347c3ca6b2c140c35871c15f7afefec4844
HDJ <263463764@qq.com>
2022-01-25 18:49:55 +0800

    移除 public -> config endpoint

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ if (window.ZEle === undefined) {
   // 生产环境（客户环境）
   // window.ZEle.endpoint = "";
   // 开发环境（本地环境)
-  window.ZEle.endpoint = "http://repo.dev.smallsaas.cn:8086";
+  window.ZEle.endpoint = "";
   window.ZEle.nav = "left"; // LeftNavCollaps left top both
   window.ZEle.indexPage = "";
   window.ZEle.breadcrumb = true;
