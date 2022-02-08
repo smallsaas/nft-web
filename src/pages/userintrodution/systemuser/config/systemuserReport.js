1ea9607ea83933c7f638907119587c59922d7a26
HDJ <263463764@qq.com>
2022-02-08 18:05:31 +0800

    处理申诉列表投入方显示问题

--- src/pages/userintrodution/systemuser/config/systemuserReport.js
+++ src/pages/userintrodution/systemuser/config/systemuserReport.js
@@ -2,40 +2,12 @@ module.exports = {
     layout: 'Content',
     title: '系统号',
     items: [
+
       {
         layout: 'Empty',
-        component: 'Search',
+        component: 'AutoReportSearch',
         config: {
           share: 'sysUserReport',
-          fields: [
-            {
-              label: '店小二',
-              type: 'select-fetch',
-              props: {
-                placeholder: '请选择',
-                style: {
-                  width: 240
-                }
-              },
-              rules: [],
-              options: {
-                API: '/api/adm/users?roleid=server',
-                label: 'name',
-                value: 'id'
-              },
-              field: 'sysUserId'
-            },
-            {
-              label: '开始时间',
-              type: 'date',
-              field: 'startDate'
-            },
-            {
-              label: '结束时间',
-              type: 'date',
-              field: 'endDate'
-            }
-          ]
         },
       },
       {
