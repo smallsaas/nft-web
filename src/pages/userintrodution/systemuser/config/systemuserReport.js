0498d35053d3664b22da64185dcd0241b9a63bc9
HDJ <263463764@qq.com>
2022-02-08 14:37:09 +0800

    玩家攻略->系统号 新增搜索栏

--- src/pages/userintrodution/systemuser/config/systemuserReport.js
+++ src/pages/userintrodution/systemuser/config/systemuserReport.js
@@ -4,6 +4,42 @@ module.exports = {
     items: [
       {
         layout: 'Empty',
+        component: 'Search',
+        config: {
+          share: 'sysUserReport',
+          fields: [
+            {
+              label: '店小二',
+              type: 'select-fetch',
+              props: {
+                placeholder: '请选择',
+                style: {
+                  width: 240
+                }
+              },
+              rules: [],
+              options: {
+                API: '/api/adm/users?roleid=server',
+                label: 'name',
+                value: 'id'
+              },
+              field: 'sysUserId'
+            },
+            {
+              label: '开始时间',
+              type: 'date',
+              field: 'startDate'
+            },
+            {
+              label: '结束时间',
+              type: 'date',
+              field: 'endDate'
+            }
+          ]
+        },
+      },
+      {
+        layout: 'Empty',
         component: 'AutoReport',
         config: {
           share: 'sysUserReport',
