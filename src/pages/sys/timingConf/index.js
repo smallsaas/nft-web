fa5217b51cbf14823de52fbb821335b4bea56792
HDJ <263463764@qq.com>
2022-03-10 09:55:45 +0800

    定时任务 新增任务历史页面

---
+++ src/pages/sys/timingConf/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Tabs } from 'antd';
+import Content from '@/layouts/Content';
+import useBreadcrumb from '@/framework/useBreadcrumb';
+import TimingConf from './timingConf';
+import TaskHistory from './taskHistory';
+
+const { TabPane } = Tabs;
+
+export default function Term(props) {
+
+  useBreadcrumb(props, [
+    { title: '首页', path: '/' },
+    { title: '系统管理' },
+    { title: '定时任务' },
+  ]);
+
+  function handleChangeTabPane(key) {
+  }
+
+  return <Content>
+    <Tabs
+      destroyInactiveTabPane
+      onChange={handleChangeTabPane}
+    >
+      <TabPane tab="执行任务" key="task_list">
+        <TimingConf />
+      </TabPane>
+      <TabPane tab="任务历史" key="task_history">
+        <TaskHistory />
+      </TabPane>
+    </Tabs>
+  </Content>
+}
